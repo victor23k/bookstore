56b7bcf897d14e3758fd04d38fc76bc0299dd8e2
victor23k <victorfdez.f@gmail.com>
2024-02-13 17:36:23 +0100

    fix compilation error in fixture

--- test/support/fixtures/catalog_fixtures.ex
+++ test/support/fixtures/catalog_fixtures.ex
@@ -44,24 +44,4 @@ defmodule Bookstore.CatalogFixtures do
 
     category
   end
-
-  @doc """
-  Generate a book.
-  """
-  def book_fixture(attrs \\ %{}) do
-    {:ok, book} =
-      attrs
-      |> Enum.into(%{
-        editor: "some editor",
-        img: "some img",
-        isbn: "some isbn",
-        price: 120.5,
-        pub_date: ~D[2024-02-11],
-        quantity: 42,
-        title: "some title"
-      })
-      |> Bookstore.Catalog.create_book()
-
-    book
-  end
 end
