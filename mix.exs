c07fb352954fce8c9ee683a9b9071e4bd8780667
victor23k <victorfdez.f@gmail.com>
2024-02-15 12:32:15 +0100

    modify reset mix alias to force drop

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Bookstore.MixProject do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.reset": ["ecto.drop --force-drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind bookstore", "esbuild bookstore"],
